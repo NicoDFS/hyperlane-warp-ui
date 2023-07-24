36206aa67ccc6fe486177235721ebfffd9045b26
constellationvault <14243318+NicoDFS@users.noreply.github.com>
2023-07-24 10:52:21 -0400

    fix link to hyperlane explorer

--- src/consts/links.ts
+++ src/consts/links.ts
@@ -1,6 +1,6 @@
 export const links = {
   home: 'https://www.kalychain.io',
-  explorer: 'https://kalyscan.io',
+  explorer: 'https://explorer.hyperlane.xyz',
   discord: 'https://discord.gg/bvtm6dUf',
   github: 'https://github.com/KalyCoinProject',
   docs: 'https://docs.kalychain.io/',
