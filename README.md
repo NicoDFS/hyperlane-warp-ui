96f65356d75ee5869b4df4f0f190fdff7d85aad6
constellationvault <14243318+NicoDFS@users.noreply.github.com>
2023-07-28 13:18:47 -0400

    bump to latest

--- README.md
+++ README.md
@@ -1,9 +1,18 @@
-# KalyBridge is a Cross-Chain Token Bridge for KalyChain
+# Hyperlane Warp Route UI Template
 
+This repo contains an example web interface for a interchain tokens built with [Hyperlane Warp Route](https://docs.hyperlane.xyz/docs/apis-and-sdks/warp-api). Warp is a framework to permisionlessly bridge tokens to any chain.
 
 ## Architecture
 
 This app is built with Next+React, Wagmi, RainbowKit, and the Hyperlane SDK.
+
+- The index page is located at `./src/pages/index.tsx`
+- The primary features are implemented in `./src/features/`
+- Constants that you may want to change are in `./src/consts/`, see the following Customization section for details.
+
+## Customization
+
+See [CUSTOMIZE.md](./CUSTOMIZE.md) for details about adjusting the tokens and branding of this app.
 
 ## Development
 
@@ -50,4 +59,8 @@ yarn clean
 
 ## Deployment
 
-The recommended hosting solution for this Next.JS app is to create a project on Vercel.
+The easiest hosting solution for this Next.JS app is to create a project on Vercel.
+
+## Learn more
+
+For more information, see the [Hyperlane documentation](https://docs.hyperlane.xyz/docs/apis-and-sdks/warp-api).
--- src/consts/chains.ts
+++ src/consts/chains.ts
@@ -1,22 +1,33 @@
-import { ChainMap } from '@hyperlane-xyz/sdk';
-
-import { CustomChainMetadata } from '../features/chains/types';
+import { ChainMap, ChainMetadataWithArtifacts, ExplorerFamily, ProtocolType } from '@hyperlane-xyz/sdk';
 
 // A map of chain names to ChainMetadata
-export const chains: ChainMap<CustomChainMetadata> = {
+export const chains: ChainMap<ChainMetadataWithArtifacts> = {
   // ----------- Add your chains here -----------------
   // Chains already in the SDK need not be included here. Example custom chain:
    kaly: {
+     protocol: ProtocolType.Ethereum,
      chainId: 3888,
+     domainId: 3888,
      name: 'kaly',
      displayName: 'KalyChain',
      nativeToken: { name: 'KalyCoin', symbol: 'KLC', decimals: 18 },
-     publicRpcUrls: [{ http: 'https://rpc.kalychain.io/rpc' }],
+     rpcUrls: [{ http: 'https://rpc.kalychain.io/rpc' }],
+     blockExplorers: [
+       {
+         name: 'KalyScan',
+         url: 'https://kalyscan.io',
+         apiUrl: 'https://kalyscan.io/api',
+         family: ExplorerFamily.Blockscout,
+       },
+     ],
      blocks: {
        confirmations: 1,
        reorgPeriod: 3,
        estimateBlockTime: 2,
      },
-     logoURI: '/kalycoin-logo.svg',
+     logoURI: 'logos/kalycoin-logo.svg',
+     interchainGasPaymaster: '0x8fbff791fCcF596DEf2e788549d0275557F95A21',
+     mailbox: '0x9FA163eF242870501Ca29CD7A082CBa2Ce24f5a2',
+     validatorAnnounce: '0x102Db287A75529c38943aCDA6A5Ff852F2647816'
    },
 };
