4c52dde50f30331ec6ba94773a603a3ec6d9a388
constellationvault <14243318+NicoDFS@users.noreply.github.com>
2023-08-03 08:16:09 -0400

    Update TipCard.tsx

--- src/components/tip/TipCard.tsx
+++ src/components/tip/TipCard.tsx
@@ -3,6 +3,7 @@ import { useState } from 'react';
 import { IconButton } from '../../components/buttons/IconButton';
 import { config } from '../../consts/config';
 import { links } from '../../consts/links';
+import InfoCircle from '../../images/icons/info-circle.svg';
 import XCircle from '../../images/icons/x-circle.svg';
 
 export function TipCard() {
